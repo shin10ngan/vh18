0c0744d127caff7a6ff517182f1e41e5ccfdd915
ChiBa <trong.vuvan@powergatesoftware.com>
2026-03-16 15:37:14 +0700

    add img

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <img src="r35.png" alt="r35" width={660} height={440} />
+      <h1>r35</h1>
+      <table border={1}>
+        <tr>
+          <th>stt</th>
+          <th>msv</th>
+          <th>ho va ten</th>
+          <th>diem</th>
+        </tr>
+        <tr>
+          <th>1</th>
+          <th>231</th>
+          <th>thieu tien tram trong</th>
+          <th>10</th>
+        </tr>
+        <tr>
+          <th>2</th>
+          <th>324</th>
+          <th>khong co tien</th>
+          <th>9</th>
+        </tr>
+      </table>
+    </>
   );
 }
 
